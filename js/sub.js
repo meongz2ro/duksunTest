67e13694a7bb84f64ffd19e6826dfc03267bd992
Hayoung <lovely10010kk@gmail.com>
2024-08-21 12:05:57 +0900

    240821

---
+++ js/sub.js
@@ -0,0 +1,7 @@
+// 서브 썸네일 클릭 시 메인 썸네일 변경
+function productThumbnail1() {
+    document.getElementById("thumbnail").src = "./images/sub/productMain.jpg";
+  }
+function productThumbnail2() {
+    document.getElementById("thumbnail").src = "./images/sub/productSub.jpg";
+  }
